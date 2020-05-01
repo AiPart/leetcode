d261546f7bec9367d49c60cbd3e3149786a8e7b8
果冻虾仁 <879231132@qq.com>
2020-05-01 23:39:58 +0800

    Create 21_1.cpp

---
+++ 14.合并两个有序链表/cpp/21_1.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummyNode;
+        ListNode *pNode = &dummyNode;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                pNode->next = l1;
+                l1 = l1->next;
+            } else {
+                pNode->next = l2;
+                l2 = l2->next;
+            }
+            pNode = pNode->next;
+        }
+        if (l1) {
+            pNode->next = l1;
+        }
+        if (l2) {
+            pNode->next = l2;
+        }
+        return dummyNode.next;
+    }
+};
